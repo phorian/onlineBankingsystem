868410fcbe9da74788dae887f124c8cf7ff9b21c
drene <jinadopeyemi@gmail.com>
2022-09-25 01:41:50 +0100

    Update
    I finished writing the remaining functions for the OBS

--- Onlinebankingsystem.c
+++ Onlinebankingsystem.c
@@ -22,11 +22,12 @@ struct user{
 };
 
 int main(){
-    struct user usr;
+    struct user usr,usr1;
     int opt,choice;
     FILE *fp;
     char filename[50],phone[50],pword[50];
     char cont = 'y';
+    float amount;
     
     printf("\n\t\t\t=================== WELCOME TO X-Nova ONLINE BANKING =================== ");
     printf("\nWhat would you Like to do? ");
@@ -62,7 +63,7 @@ int main(){
     if (opt == 2) {
         system("cls");
         printf("\n\t\tX-NOVA BANK");
-        printf("\nPhone Number: ");
+        printf("\n\nPhone Number: ");
         scanf("%s", &phone);
         printf("\nPassword: ");
         scanf("%s", &pword);
@@ -78,6 +79,8 @@ int main(){
                 while (cont == 'y')
                 {
                     system("cls");
+                    printf("\n\t\tX-NOVA BANK");
+                    printf("\n\nHello %s", usr.FullName);
                     printf("\n\n1. Check balance");
                     printf("\n\n2. Deposit Funds");
                     printf("\n\n3. Withdraw Funds");
@@ -89,16 +92,94 @@ int main(){
 
                     switch (choice)
                     {
-                    case 1:
-                        printf("\n Your current Balance is NGN%.2f", usr.balance);
+                        case 1:
+                            printf("\n Your current Balance is NGN%.2f", usr.balance);
                         break;
-                    
-                    default:
+                        case 2:
+                            printf("\nEnter the amount to deposit: \t");
+                            scanf("%f", &amount);
+                            usr.balance += amount;
+                            fp = fopen(filename, "w");
+                            fwrite(&usr, sizeof(struct user),1,fp);
+                            if (fwrite != NULL)
+                             {
+                                printf("\n\tYou have deposited NGN%.2f", amount);
+                                printf("\n\tYour balance is NGN%.2f", usr.balance);
+                                printf("\n\t\tThank you for banking with us");
+                            }
+                             fclose(fp);
+                         break;
+                        case 3:
+                            printf("\nEnter the amount to withdraw: \t");
+                            scanf("%f", &amount);
+                            usr.balance -= amount;
+                            fp = fopen(filename, "w");
+                            fwrite(&usr, sizeof(struct user),1,fp);
+                            if (fwrite != NULL)
+                             {
+                                printf("\n\tYou have Withdrawn NGN%.2f", amount);
+                                printf("\n\tYour balance is NGN%.2f", usr.balance);
+                                printf("\n\t\tThank you for banking with us");
+                             }
+                             fclose(fp);
                         break;
-                    }
+                        case 4:
+                            printf("Please Enter phone number to transfer into: \t");
+                            scanf("%s", phone);
+                            printf("Please enter amount to transfer: \t");
+                            scanf("%f", &amount);
+                            strcpy(filename, phone);
+                                fp = fopen(strcat(filename, ".dat"), "r");
+                                if(fp == NULL){
+                                    printf("\nAccount does not exist");
+                                    return 0;
+                                }
+                            if(amount > usr.balance)
+                                printf("Insufficient Funds");
+                             else 
+                            {
+                                fread(&usr1, sizeof(struct user),1,fp);
+                                fclose(fp);
+                                fp = fopen(filename, "w");
+                                usr1.balance += amount;
+                                fwrite(&usr1,sizeof(struct user),1,fp);
+                                fclose(fp);
+                                if (fwrite != 0)
+                                 {
+                                    printf("Transfer Succesful");
+                                    strcpy(filename,usr.phone);
+                                    fp = fopen(strcat(filename,".dat"), "w");
+                                    usr.balance -= amount;
+                                    fwrite(&usr,sizeof(struct user),1,fp);
+                                    fclose(fp);
 
-                    printf("\n\n\t\tDo you want to perform another transaction?...[y/n]");
+                                 }
+
+                            }
+                        break;
+
+                         case 5:
+                            printf("\nPlease enter your new password: \t");
+                            scanf("%s", pword);
+                            fp = fopen(filename, "w");
+                            strcpy(usr.password,pword);
+                            fwrite(&usr, sizeof(struct user),1,fp);
+                            if ((fwrite != NULL))
+                            {
+                                printf("Password succesfully changed");
+                            }
+                        break;
+
+                    default:
+                        printf("Invalid Option");
+                    break;
+
+                    }
+                        
+
+                    printf("\n\n\t\tDo you want to perform another transaction?...[y/n]: ");
                     scanf("%s", &cont);
+
                 }
             }
              else 
